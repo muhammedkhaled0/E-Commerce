2411f9417a4dc0cb0c1ede4fb3dc90454e084934
Muhammed Elsiny <muhammedkhaled7882@gmail.com>
2025-12-27 19:40:00 +0200

    last

--- src/components/PaymentDialog.tsx
+++ src/components/PaymentDialog.tsx
@@ -29,7 +29,7 @@ export function PaymentDialog({cartId}:{cartId:string}) {
     setIsLoading(true)
     console.log(cartId);
     const shippingAddress={details:userDetails.current?.value,phone:userPhone.current?.value,city:userCity.current?.value}
-    const res=await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${process.env.NEXTAUTH_URL}`,{
+    const res=await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://e-commerce-five-iota-63.vercel.app`,{
     method:'POST',
     body:JSON.stringify({shippingAddress}),
     headers:{
