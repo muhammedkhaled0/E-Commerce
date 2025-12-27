ae5d743d761e9f6179631909b564e9a36686034d
Muhammed Elsiny <muhammedkhaled7882@gmail.com>
2025-12-27 19:14:27 +0200

    token

--- src/app/Helpers/getUserToken.ts
+++ src/app/Helpers/getUserToken.ts
@@ -1,7 +1,8 @@
  import { cookies } from 'next/headers'
 import { decode } from 'next-auth/jwt'
 export async function getUserToken(){
-  const x=(await cookies()).get('next-auth.session-token')?.value
-  const accessToken=await decode({token:x,secret:process.env.NEXTAUTH_SECRET!})
+  const myCookies=await cookies()
+  const decodedToken=myCookies.get('next-auth.session-token')?.value||myCookies.get('__secure-next-auth.session-token')?.value
+  const accessToken=await decode({token:decodedToken,secret:process.env.NEXTAUTH_SECRET!})
   return accessToken?.token
 }
